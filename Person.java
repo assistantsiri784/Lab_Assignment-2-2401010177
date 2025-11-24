324f00f0ed0fe35de1e7acae1e95634e55dfb8f9
Krish Singhal <krishsinghal784@gmail.com>
2025-11-24 18:20:49 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,15 @@
+package lassignment2;
+
+public abstract class Person {
+    protected String name;
+    protected String email;
+
+    public Person() {}
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public abstract void displayInfo();
+}
